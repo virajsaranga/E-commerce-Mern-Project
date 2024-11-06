63256afe1e47ac1980d2a2c065050cc8692edc15
virajsaranga <virajridi@gmail.com>
2024-11-06 22:43:22 +0530

    api slice file updated

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -6,6 +6,6 @@ const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ['User','Product', 'Order'],
+  tagTypes: ["Product", "Order", "User"],
   endpoints: (builder) => ({}),
 });
