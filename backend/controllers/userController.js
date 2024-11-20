4201da273589d0b38d7bf9aacb84df3d060de98d
virajsaranga <virajridi@gmail.com>
2024-11-20 21:44:35 +0530

    user controller updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,39 +81,6 @@
 //   }
 // });
 
-// // @desc    Update user profile
-// // @route   PUT /api/users/profile
-// // @access  Private
-// const updateUserProfile = asyncHandler(async (req, res) => {
-//   const user = await User.findById(req.user._id);
-
-//   if (user) {
-//     user.name = req.body.name || user.name;
-//     user.email = req.body.email || user.email;
-
-//     if (req.body.password) {
-//       user.password = req.body.password;
-//     }
-
-//     const updatedUser = await user.save();
-
-//     res.json({
-//       _id: updatedUser._id,
-//       name: updatedUser.name,
-//       email: updatedUser.email,
-//     });
-//   } else {
-//     res.status(404);
-//     throw new Error('User not found');
-//   }
-// });
-// export {
-//   authUser,
-//   registerUser,
-//   logoutUser,
-//   getUserProfile,
-//   updateUserProfile,
-// };
 
 
 import asyncHandler from 'express-async-handler';
